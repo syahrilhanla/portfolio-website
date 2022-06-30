1cd45d2bee38bab141f13fbb8264a6d4a510cf70
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-30 08:57:16 +0800

    migrate to double quote

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,7 +2,7 @@ import Introduction from "./Introduction";
 
 const Home = () => {
 	return (
-		<div className='bg-[#0c2852] w-full min-h-screen flex flex-col items-center'>
+		<div className="bg-[#0c2852] w-full min-h-screen flex flex-col items-center">
 			<Introduction />
 		</div>
 	);
