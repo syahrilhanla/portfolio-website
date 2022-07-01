fa4c518b221c92f453588bb823a536ed73aa6b53
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-01 08:06:20 +0800

    added instagram link

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -1,4 +1,4 @@
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsPersonLinesFill } from "react-icons/bs";
 import Link from "next/link";
@@ -11,7 +11,7 @@ const SocialIcons = ({ navbar }) => {
 			}
 		>
 			<ul>
-				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-90px] hover:ml-[-5px] duration-300">
+				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-100px] hover:ml-[-5px] duration-300">
 					<Link href="https://www.linkedin.com/in/syahril-hanla-45387220a/">
 						<a
 							target="_blank"
@@ -21,7 +21,7 @@ const SocialIcons = ({ navbar }) => {
 						</a>
 					</Link>
 				</li>
-				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-90px] hover:ml-[-5px] duration-300">
+				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-100px] hover:ml-[-5px] duration-300">
 					<Link href="https://github.com/syahrilhanla">
 						<a
 							target="_blank"
@@ -31,14 +31,24 @@ const SocialIcons = ({ navbar }) => {
 						</a>
 					</Link>
 				</li>
-				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-90px] hover:ml-[-5px] duration-300">
+				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-100px] hover:ml-[-5px] duration-300">
+					<Link href="https://www.instagram.com/syahrilhanla64/">
+						<a
+							target="_blank"
+							className="flex justify-between items-center w-full text-gray-300"
+						>
+							Instagram <FaInstagram size={30} />
+						</a>
+					</Link>
+				</li>
+				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-100px] hover:ml-[-5px] duration-300">
 					<Link href="/">
 						<a className="flex justify-between items-center w-full text-gray-300">
 							Email <HiOutlineMail size={30} />
 						</a>
 					</Link>
 				</li>
-				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-90px] hover:ml-[-5px] duration-300">
+				<li className="h-[60px] w-[150px] flex justify-between items-center ml-[-100px] hover:ml-[-5px] duration-300">
 					<Link href="/">
 						<a className="flex justify-between items-center w-full text-gray-300">
 							Contact <BsPersonLinesFill size={30} />
