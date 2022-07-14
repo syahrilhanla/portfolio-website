b0cda056b1def07e652095cd6cdc2ee55f12826c
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-14 21:43:39 +0800

    added responsiveness to gray card

--- components/Works.jsx
+++ components/Works.jsx
@@ -8,18 +8,68 @@ const Works = () => {
 			title: "CyberAni",
 			repoAddress: "https://github.com/syahrilhanla/cyberani",
 			projectAddress: "https://cyberani.vercel.app/",
+			description:
+				"CyberAni is a free anime streaming platform built with React, Next.js,and TailwindCSS. It is a private project for educational purposes only. Thanks to @riimuru for the API.",
+			techStack: [
+				{
+					img: "/react.png",
+					name: "React",
+				},
+				{
+					img: "/nextJS.svg",
+					name: "Next.js",
+				},
+				{
+					img: "/tailwind.png",
+
+					name: "TailwindCSS",
+				},
+			],
 		},
 		{
 			imgURL: "ia-statistics.png",
 			title: "I/A Statistics",
 			repoAddress: "https://github.com/syahrilhanla/proyek-skripsi",
 			projectAddress: "https://ia-statistics.vercel.app/",
+			description:
+				"CyberAni is a free anime streaming platform built with React, Next.js,and TailwindCSS. It is a private project for educational purposes only. Thanks to @riimuru for the API.",
+			techStack: [
+				{
+					img: "/react.png",
+					name: "React",
+				},
+				{
+					img: "/nextJS.svg",
+					name: "Next.js",
+				},
+				{
+					img: "/tailwind.png",
+
+					name: "TailwindCSS",
+				},
+			],
 		},
 		{
 			imgURL: "shopping-cart.png",
 			title: "Redux Shopping Cart",
 			repoAddress: "https://github.com/syahrilhanla/shopping-cart",
 			projectAddress: "https://syahrilhanla.github.io/shopping-cart/",
+			description:
+				"CyberAni is a free anime streaming platform built with React, Next.js,and TailwindCSS. It is a private project for educational purposes only. Thanks to @riimuru for the API.",
+			techStack: [
+				{
+					img: "/react.png",
+					name: "React",
+				},
+				{
+					img: "/nextJS.svg",
+					name: "Next.js",
+				},
+				{
+					img: "/tailwind.png",
+					name: "TailwindCSS",
+				},
+			],
 		},
 
 		// {
@@ -32,8 +82,9 @@ const Works = () => {
 
 	const DetailCard = () => (
 		<div
-			className="bg-slate-800 text-slate-200 h-fit w-full 
-		absolute bottom-0 left-0 py-2 rounded-sm group-hover:h-fit"
+			className="bg-slate-800 text-slate-200 md:h-fit md:w-full md:bottom-[-25%]
+			 md:absolute w-[85vw]
+		  left-0 py-2 rounded-sm md:group-hover:bottom-0 duration-300"
 		>
 			<h2 className="font-semibold text-2xl">CyberAni</h2>
 			<h4 className="font-normal text-xl">Techstack:</h4>
@@ -51,7 +102,7 @@ const Works = () => {
 					<p className="text-base font-light">TailwindCSS</p>
 				</div>
 			</div>
-			<div className="group-hover:block hidden overflow-hidden px-3">
+			<div className="px-3 md:group-hover:visible md:invisible md:group-hover:block">
 				<h4 className="font-normal text-xl">Description: </h4>
 				<p className="text-base font-light">
 					CyberAni is a free anime streaming platform built with React, Next.js,
@@ -67,7 +118,7 @@ const Works = () => {
 			name="works"
 			className="bg-[#0c2852] min-h-screen text-gray-300 flex flex-col justify-center items-center text-center md:px-[8rem] px-[2rem] pb-7"
 		>
-			<div className="flex justify-start flex-col sm:mt-3 mt-[6rem]">
+			<div className="flex justify-start flex-col sm:mt-3 mt-[6rem] md:w-[70vw] w-[85vw]">
 				<h1 className="text-4xl w-min font-medium border-b-4 border-pink-600 py-1">
 					Works
 				</h1>
@@ -78,21 +129,12 @@ const Works = () => {
 				<div className="flex flex-wrap">
 					{workImages.map((image) => (
 						<div
-							style={{ backgroundImage: `url(${image.imgURL})` }}
-							className="relative shadow-lg shadow-[#040c16] group container
-							 rounded-md flex justify-center items-center mx-auto
-							  w-[55%] bg-no-repeat bg-center bg-cover min-h-[480px] z-0 m-8"
+							className="shadow-lg shadow-[#040c16] group container
+							 rounded-md mx-auto md:relative flex items-end md:overflow-hidden
+							 md:w-[400px] w-[100vw] bg-no-repeat bg-center bg-cover min-h-[480px] m-4"
 							key={image.title}
+							style={{ backgroundImage: `url(${image.imgURL})` }}
 						>
-							{/* <div
-								className=" absolute top-0 left-0 bg-gray-900 bg-no-repeat bg-center bg-cover 
-								h-full w-full opacity-0 group-hover:opacity-70 duration-300"
-							/> */}
-							<div className="opacity-0 group-hover:opacity-0 group-hover:z-50 ">
-								<span className="text-3xl font-light text-white tracking-wider sm:p-4">
-									{image.title}
-								</span>
-							</div>
 							<DetailCard />
 						</div>
 					))}
